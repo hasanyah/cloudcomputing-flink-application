26e886c3030b02fb29fa7e28ae68fd7a3ac3e9e2
Hasan Yahya <hasanyah@gmail.com>
2021-11-16 14:19:21 +0100

    Improve readability via inheritance and cleaning up the code

--- VehicleTelematics/src/main/java/es/upm/master/VehicleTelematics.java
+++ VehicleTelematics/src/main/java/es/upm/master/VehicleTelematics.java
@@ -2,6 +2,7 @@ package es.upm.master;
 
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple6;
@@ -16,8 +17,13 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.TaskManagerOptions;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import java.util.Iterator;
 
 public class VehicleTelematics {
+
+    static final Integer SPEED_LIMIT = 20;
     public static void main(String[] args) {
         
         final ParameterTool params = ParameterTool.fromArgs(args);
@@ -33,14 +39,13 @@ public class VehicleTelematics {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(parallelism, configuration);
         */
         
-        DataStreamSource<String> s2 = env.readTextFile(params.get("input"));
+        DataStreamSource<String> inputStream = env.readTextFile(params.get("input"));
 
         env.getConfig().setGlobalJobParameters(params);
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-        SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> s3 = 
-        s2.flatMap(new FlatMapFunction<String, Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>>() {
-            public void flatMap(String input, Collector<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> collector) throws Exception {
+        SingleOutputStreamOperator<CarData> inputMap = inputStream.flatMap(new FlatMapFunction<String, CarData>() {
+            public void flatMap(String input, Collector<CarData> collector) throws Exception {
                 String[] dataArray = input.split("\n");
                 for (String line : dataArray) {
                     String[] data = line.split(",");
@@ -48,87 +53,63 @@ public class VehicleTelematics {
                     for (int i = 0; i < data.length; i++) {
                         intData[i] = Integer.parseInt(data[i]);
                     }
-
-                    collector.collect(new Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>(intData[0], intData[1], intData[2], intData[3], intData[4], intData[5], intData[6], intData[7] ));
+                    collector.collect(new CarData(intData[0], intData[1], intData[2], intData[3], intData[4], intData[5], intData[6], intData[7]));
                 }
             }
         });
 
-        KeyedStream<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>, Tuple> keyedStream = s3.keyBy(1);
-        
-        
         // Dealing with the speed radar: cars with speed >= 90
         // store the Time[0], VID[1], XWay[3], Seg[6], Dir[5], Spd[2]
-        SingleOutputStreamOperator<Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>> speedFilteredCars = s3.filter(new FilterFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>>() {
+        SingleOutputStreamOperator<CarData> speedFilteredCars = inputMap.filter(new FilterFunction<CarData>() {
             @Override
-            public boolean filter(Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> in) throws Exception {
-                return in.f2 >= 20; 
+            public boolean filter(CarData in) throws Exception {
+                return in.f2 >= SPEED_LIMIT; 
             }
         });
 
-        // SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> s5 = s4.reduce(new ReduceFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>>() {
-        //     @Override
-        //     public Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> reduce(Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> accumulator, Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> input) throws Exception {
-        //         return new Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>(input.f0, input.f1, input.f2, input.f3, input.f4, input.f5, input.f6, input.f7);
-        //     }
-        // });
-
-
-
-        // SingleOutputStreamOperator<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> s5 = s4.reduce(new ReduceFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>>() {
-        //     public Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> reduce(Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> accumulator, Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> input) throws Exception {
-        //         return new Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>(accumulator.f0, accumulator.f1, accumulator.f2, accumulator.f3, accumulator.f4, accumulator.f5, accumulator.f6, accumulator.f7);
-        //     }
-        // });
+        // Remove the unnecessary columns
+        SingleOutputStreamOperator<SpeedRadarData> speedRadarData = 
+        speedFilteredCars.map(new MapFunction<CarData, SpeedRadarData>() {
+            public SpeedRadarData map(CarData in) throws Exception{
+                return new SpeedRadarData(in.f0, in.f1, in.f3, in.f6, in.f5, in.f2);
+            }
+        });
 
         // emit result
         if (params.has("output")) {
-            speedFilteredCars.writeAsText(params.get("output"), FileSystem.WriteMode.OVERWRITE);
+            speedRadarData.writeAsText("speedfines.csv", FileSystem.WriteMode.OVERWRITE);
         }
         else {
             System.out.println("Printing result to stdout. Use --output to specify output path.");
-            speedFilteredCars.print();
+            speedRadarData.print();
         }
 
         try {
             env.execute();
         } catch (Exception e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
+        KeyedStream<CarData, Tuple> keyedStream = inputMap.keyBy(1);
     }
 
-    public class CarData extends Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> {
+    public static class CarData extends Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> {
         public CarData() {
+            super();
+        }
+
+        public CarData(Integer time, Integer vid, Integer spd, Integer xway, Integer lane, Integer dir, Integer seg, Integer pos) {
             super();
         }
     }
 
-    public static class SimpleSum implements WindowFunction<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>, Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>, Tuple, GlobalWindow> {
-        public void apply(Tuple tuple, GlobalWindow countWindow, Iterable<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> input, Collector<Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>> out) throws Exception {
-            Iterator<Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer>> iterator = input.iterator();
-            Tuple8<Integer, Integer, Integer, Integer, Integer, Integer, Integer, Integer> first = iterator.next();
-            Integer time;
-            Integer vid;
-            Integer spd;
-            Integer xway;
-            Integer dir;
-            Integer seg;
-            
-            if(first!=null){
-                time = first.f0;
-                vid = first.f1;
-                spd = first.f2;
-                xway = first.f3;
-                dir = first.f5;
-                seg = first.f6;
-            }
-            // while(iterator.hasNext()){
-            //     Tuple3<Long, String, Double> next = iterator.next();
-            //     ts = next.f0;
-            //     temp += next.f2;
-            // }
-            out.collect(new Tuple6<Integer, Integer, Integer, Integer, Integer, Integer>(time, vid, spd, xway, dir, seg));
+    public static class SpeedRadarData extends Tuple6<Integer, Integer, Integer, Integer, Integer, Integer> {
+        public SpeedRadarData() {
+            super();
+        }
+
+        public SpeedRadarData(Integer time, Integer vid, Integer xway, Integer seg, Integer dir, Integer spd) {
+            super();
         }
     }
 }
