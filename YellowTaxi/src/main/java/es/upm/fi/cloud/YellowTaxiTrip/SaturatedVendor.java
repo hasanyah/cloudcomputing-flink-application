c6c762cfaaa31985c72e82cd7ce8f9745e6f76cd
Hasan Yahya <hasanyah@gmail.com>
2022-06-28 17:31:56 +0200

    Implement a custom trigger that will fire for countOfElements >= 2

--- YellowTaxi/src/main/java/es/upm/fi/cloud/YellowTaxiTrip/SaturatedVendor.java
+++ YellowTaxi/src/main/java/es/upm/fi/cloud/YellowTaxiTrip/SaturatedVendor.java
@@ -47,6 +47,9 @@ import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.api.common.state.ReducingState;
+import org.apache.flink.api.common.state.ReducingStateDescriptor;
+import org.apache.flink.api.common.typeutils.base.LongSerializer;
 
 public class SaturatedVendor {
     public static void main(String[] args){
@@ -72,7 +75,7 @@ public class SaturatedVendor {
 			}
 		}).keyBy(0)
 		.window(GlobalWindows.create())
-		.trigger(CountTrigger.of(2))
+		.trigger(CustomCountTrigger.of(2))
 		.evictor(new Evictor<Tuple4<Integer, Long, Long, Integer>, GlobalWindow>() {
 			@Override
             public void evictBefore(Iterable<TimestampedValue<Tuple4<Integer, Long, Long, Integer>>> elements, int size, GlobalWindow window, EvictorContext evictorContext) {
@@ -81,9 +84,10 @@ public class SaturatedVendor {
 			@Override
             public void evictAfter(Iterable<TimestampedValue<Tuple4<Integer, Long, Long, Integer>>> elements, int size, GlobalWindow window, EvictorContext evictorContext) {
 				Iterator<TimestampedValue<Tuple4<Integer, Long, Long, Integer>>> iterator = elements.iterator();
-				if (iterator.hasNext()) 
+				if (iterator.hasNext()) {
 					iterator.next();
 					iterator.remove();
+				}
             }
         })
 		.apply(new WindowFunction<Tuple4<Integer, Long, Long, Integer>, Tuple4<Integer, String, String, Integer>,Tuple, GlobalWindow>() {
@@ -129,5 +133,78 @@ public class SaturatedVendor {
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		return formatter.parse(sDate).getTime();
+	}
+
+	public static class CustomCountTrigger<W extends Window> extends Trigger<Object, W> {
+		private static final long serialVersionUID = 1L;
+	
+		private final long maxCount;
+	
+		private final ReducingStateDescriptor<Long> stateDesc =
+				new ReducingStateDescriptor<>("count", new Sum(), LongSerializer.INSTANCE);
+	
+		private CustomCountTrigger(long maxCount) {
+			this.maxCount = maxCount;
+		}
+	
+		@Override
+		public TriggerResult onElement(Object element, long timestamp, W window, TriggerContext ctx) throws Exception {
+			ReducingState<Long> count = ctx.getPartitionedState(stateDesc);
+			count.add(1L);
+			if (count.get() >= maxCount) {
+				return TriggerResult.FIRE;
+			}
+			return TriggerResult.CONTINUE;
+		}
+	
+		@Override
+		public TriggerResult onEventTime(long time, W window, TriggerContext ctx) {
+			return TriggerResult.CONTINUE;
+		}
+	
+		@Override
+		public TriggerResult onProcessingTime(long time, W window, TriggerContext ctx) throws Exception {
+			return TriggerResult.CONTINUE;
+		}
+	
+		@Override
+		public void clear(W window, TriggerContext ctx) throws Exception {
+			ctx.getPartitionedState(stateDesc).clear();
+		}
+	
+		@Override
+		public boolean canMerge() {
+			return true;
+		}
+	
+		@Override
+		public void onMerge(W window, OnMergeContext ctx) throws Exception {
+			ctx.mergePartitionedState(stateDesc);
+		}
+	
+		@Override
+		public String toString() {
+			return "CustomCountTrigger(" +  maxCount + ")";
+		}
+	
+		/**
+		 * Creates a trigger that fires once the number of elements in a pane reaches the given count.
+		 *
+		 * @param maxCount The count of elements at which to fire.
+		 * @param <W> The type of {@link Window Windows} on which this trigger can operate.
+		 */
+		public static <W extends Window> CustomCountTrigger<W> of(long maxCount) {
+			return new CustomCountTrigger<>(maxCount);
+		}
+	
+		private static class Sum implements ReduceFunction<Long> {
+			private static final long serialVersionUID = 1L;
+	
+			@Override
+			public Long reduce(Long value1, Long value2) throws Exception {
+				return value1 + value2;
+			}
+	
+		}
 	}
 }	
